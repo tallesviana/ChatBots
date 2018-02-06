4d126f6054ddd9d09dc2b9cdcc0f6ce1a20eb81a
tallesviana <tallesviana1@gmail.com>
2018-02-05 22:52:05 -0200

    Implementation: Added QnaSimpleDialog linked to Bula.i / Working

--- BulaBot/BulaBot/Controllers/MessagesController.cs
+++ BulaBot/BulaBot/Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace BulaBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.QnASimpleDialog());
             }
             else
             {
---
+++ BulaBot/BulaBot/Dialogs/QnaSimpleDialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using Microsoft.Bot.Builder.Dialogs;
+using QnAMakerDialog;
+
+namespace BulaBot.Dialogs
+{
+    [Serializable]
+    //[QnAMaker("2534312576ca45ca8c90133186443165", "24963aa4-3043-41d3-b582-3f06f2d5db83","Desculpa, nao entendi.", 0.5,3)]
+    //public class QnaBotwithActivedLearning : Qna
+    [QnAMakerService("2534312576ca45ca8c90133186443165", "24963aa4-3043-41d3-b582-3f06f2d5db83")]
+    public class QnASimpleDialog : QnAMakerDialog<object>
+    {
+        public override async Task NoMatchHandler(IDialogContext context, string original)
+        {
+            await context.PostAsync($"Desculpe, mas nao entendi o que voce quis dizer com: '{original}'.");
+            context.Wait(MessageReceived);
+        }
+
+       
+    }
+}
